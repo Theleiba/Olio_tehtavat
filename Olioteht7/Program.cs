687f53c9edc8adf36f9cd2c0f4bb455b182bacc5
Theleiba <henri.pollari@gmail.com>
2016-01-19 19:22:04 +0200

    Tehtävä 7

---
+++ Olioteht7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olioteht7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vuosi;
+            Console.WriteLine("Anna vuosi: ");
+            vuosi = int.Parse(Console.ReadLine());
+            if (vuosi % 4 == 0)
+            {
+                if (vuosi % 100 == 0)
+                {
+                    if (vuosi % 400 == 0)
+                        Console.WriteLine("Vuosi on karkausvuosi.");
+                    else
+                        Console.WriteLine("Vuosi ei ole karkausvuosi.");
+                }
+                else
+                    Console.WriteLine("Vuosi on karkausvuosi.");
+            }
+            else
+                Console.WriteLine("Vuosi ei ole karkausvuosi.");
+        }
+    }
+}
