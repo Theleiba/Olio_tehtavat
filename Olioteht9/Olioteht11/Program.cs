00c4582cae8a0cea1237f976734c32ec7c8e437a
Henri Pollari <K2930@student.jamk.fi>
2016-01-20 00:29:09 +0200

    Tehtävä 11

---
+++ Olioteht9/Olioteht11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olioteht11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku;
+            int luku2;
+            Console.WriteLine("Anna luku ");
+            luku = int.Parse(Console.ReadLine());
+            luku2 = luku;
+            for (int i = 0;i < luku;i++, luku2 = luku - i)
+            {
+                for (; luku2 <= luku;luku2++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
