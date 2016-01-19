7496c69139e90dbb6ed398ead121665c702a8abc
Henri Pollari <K2930@student.jamk.fi>
2016-01-19 23:07:07 +0200

    Tehtävä 10

---
+++ Olioteht10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olioteht10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luvut = { 1, 2, 33, 44, 55, 68, 77, 96, 100 };
+            for (int i = 0 ; i < 9 ; i++)
+            {
+                Console.WriteLine(luvut[i]);
+                if (luvut[i] % 2 == 0)
+                    Console.WriteLine("HEP!");
+            }
+        }
+    }
+}
