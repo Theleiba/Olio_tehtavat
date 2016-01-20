237f248344d98c5243e62f69bece803d9d291f00
Theleiba <henri.pollari@gmail.com>
2016-01-20 18:07:58 +0200

    tehtävä 12 aloitus

---
+++ Olioteht12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olioteht12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbas = { 0 };
+            Console.WriteLine("Anna luku ");
+            numbas[0] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna luku ");
+            numbas[1] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna luku ");
+            numbas[2] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna luku ");
+            numbas[3] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna luku ");
+            numbas[4] = int.Parse(Console.ReadLine());
+        }
+    }
+}
